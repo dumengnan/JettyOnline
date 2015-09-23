10b86f101486235b7ae864a92ef3ef33ee64fdf1
dci <dci@1107593797@qq.com>
2015-09-23 08:47:36 +0800

    database user table

---
+++ user.sql
@@ -0,0 +1,14 @@
+create database project;
+
+use project;
+
+create table user
+(
+	id int primary key auto_increment,
+	username varchar(40) not null,
+	password varchar(16) not null,
+	company  varchar(255) not null,
+	gender int not null
+);
+
+insert into user values(0,'hello', 'world','nanjinglzy', 1);
